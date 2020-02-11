8d16a3a37f2b4dcd9d8d55a58a9789d16be1d81c
Harlor <dominik.wille@erdfisch.de>
2020-02-11 11:17:36 +0100

    Introduce moderation action plugins

---
+++ src/Plugin/Moderation/EventDispatcherModerationAction.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Plugin implementation of the 'text_plain' formatter.
+ *
+ * @ModerationAction(
+ *   id = "text_plain",
+ *   label = @Translation("Plain text"),
+ *   field_types = {
+ *     "text",
+ *     "text_long",
+ *     "text_with_summary",
+ *   },
+ *   edit = {
+ *     "editor" = "direct",
+ *   },
+ * )
+ */
+class EventDispatcherAction {
+
+}
---
+++ src/Plugin/ModerationActionInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+
+namespace Drupal\moderation\Plugin;
+
+
+interface ModerationActionInterface {
+  public function action();
+}
