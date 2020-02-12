a0e109105124e8d9c06bd7cfed7eb7ec3b81c425
Harlor <dominik.wille@erdfisch.de>
2020-02-12 14:19:34 +0100

    Init moderation views integration

---
+++ src/Plugin/views/field/ModerationAction.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * @file
+ * Definition of Drupal\d8views\Plugin\views\field\NodeTypeFlagger
+ */
+
+namespace Drupal\moderation\Plugin\views\field;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\node\Entity\NodeType;
+use Drupal\views\Plugin\views\field\FieldPluginBase;
+use Drupal\views\ResultRow;
+
+/**
+ * Field handler to flag the node type.
+ *
+ * @ingroup views_field_handlers
+ *
+ * @ViewsField("moderation_handler_actions_field")
+ */
+class ModerationAction extends FieldPluginBase {
+
+  /**
+   * @{inheritdoc}
+   */
+  public function query() {
+    // Leave empty to avoid a query on this field.
+  }
+
+  /**
+   * Define the available options
+   * @return array
+   */
+  protected function defineOptions() {
+    $options = parent::defineOptions();
+    $options['node_type'] = array('default' => 'article');
+
+    return $options;
+  }
+
+  /**
+   * Provide the options form.
+   */
+  public function buildOptionsForm(&$form, FormStateInterface $form_state) {
+    $types = NodeType::loadMultiple();
+    $options = [];
+    foreach ($types as $key => $type) {
+      $options[$key] = $type->label();
+    }
+    $form['node_type'] = array(
+      '#title' => $this->t('Which node type should be flagged?'),
+      '#type' => 'select',
+      '#default_value' => $this->options['node_type'],
+      '#options' => $options,
+    );
+
+    parent::buildOptionsForm($form, $form_state);
+  }
+
+  /**
+   * @{inheritdoc}
+   */
+  public function render(ResultRow $values) {
+    $entity = $values->_entity;
+    $moderation_types = \Drupal::entityTypeManager()->getStorage('moderation_type')->loadMultiple();
+    foreach ($moderation_types as $moderation_type) {
+      if ($moderation_type->entityIsModerated($entity)) {
+        return $moderation_type->actionLinks($entity);
+      }
+    }
+  }
+}
