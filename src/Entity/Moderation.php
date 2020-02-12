e40390d12e9e12139fbed5b062022cd3e13b885f
Harlor <dominik.wille@erdfisch.de>
2020-02-12 14:17:41 +0100

    Moderation entity

---
+++ src/Entity/Moderation.php
@@ -0,0 +1,182 @@
+<?php
+
+namespace Drupal\moderation\Entity;
+
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Field\BaseFieldDefinition;
+use Drupal\Core\Entity\ContentEntityBase;
+use Drupal\Core\Entity\EntityChangedTrait;
+use Drupal\Core\Entity\EntityPublishedTrait;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\user\UserInterface;
+
+/**
+ * Defines the Moderation entity.
+ *
+ * @ingroup moderation
+ *
+ * @ContentEntityType(
+ *   id = "moderation",
+ *   label = @Translation("Moderation"),
+ *   handlers = {
+ *     "view_builder" = "Drupal\Core\Entity\EntityViewBuilder",
+ *     "list_builder" = "Drupal\moderation\ModerationListBuilder",
+ *     "views_data" = "Drupal\moderation\Entity\ModerationViewsData",
+ *
+ *     "access" = "Drupal\moderation\ModerationAccessControlHandler",
+ *   },
+ *   base_table = "moderation",
+ *   translatable = FALSE,
+ *   admin_permission = "administer moderation entities",
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "uuid" = "uuid",
+ *     "uid" = "user_id",
+ *     "published" = "status",
+ *   },
+ * )
+ */
+class Moderation extends ContentEntityBase implements ModerationInterface {
+
+  use EntityChangedTrait;
+  use EntityPublishedTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function preCreate(EntityStorageInterface $storage_controller, array &$values) {
+    parent::preCreate($storage_controller, $values);
+    $values += [
+      'user_id' => \Drupal::currentUser()->id(),
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getName() {
+    return $this->get('name')->value;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setName($name) {
+    $this->set('name', $name);
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCreatedTime() {
+    return $this->get('created')->value;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setCreatedTime($timestamp) {
+    $this->set('created', $timestamp);
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getOwner() {
+    return $this->get('user_id')->entity;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getOwnerId() {
+    return $this->get('user_id')->target_id;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setOwnerId($uid) {
+    $this->set('user_id', $uid);
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setOwner(UserInterface $account) {
+    $this->set('user_id', $account->id());
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
+    $fields = parent::baseFieldDefinitions($entity_type);
+
+    // Add the published field.
+    $fields += static::publishedBaseFieldDefinitions($entity_type);
+
+    $fields['user_id'] = BaseFieldDefinition::create('entity_reference')
+      ->setLabel(t('Authored by'))
+      ->setDescription(t('The user ID of author of the Moderation entity.'))
+      ->setRevisionable(TRUE)
+      ->setSetting('target_type', 'user')
+      ->setSetting('handler', 'default')
+      ->setDisplayOptions('view', [
+        'label' => 'hidden',
+        'type' => 'author',
+        'weight' => 0,
+      ])
+      ->setDisplayOptions('form', [
+        'type' => 'entity_reference_autocomplete',
+        'weight' => 5,
+        'settings' => [
+          'match_operator' => 'CONTAINS',
+          'size' => '60',
+          'autocomplete_type' => 'tags',
+          'placeholder' => '',
+        ],
+      ])
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayConfigurable('view', TRUE);
+
+    $fields['type'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('Moderation type'))
+      ->setRequired(TRUE)
+      ->setDescription(t('The bundle of this entity.'))
+      ->setSetting('is_ascii', TRUE)
+      ->setSetting('max_length', EntityTypeInterface::ID_MAX_LENGTH);
+
+    $fields['entity_type'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('Entity type'))
+      ->setRequired(TRUE)
+      ->setDescription(t('The entity type which is moderated.'))
+      ->setSetting('is_ascii', TRUE)
+      ->setSetting('max_length', EntityTypeInterface::ID_MAX_LENGTH);
+
+    $fields['entity_id'] = BaseFieldDefinition::create('entity_reference')
+      ->setLabel(t('Entity ID'))
+      ->setDescription(t('The ID of the entity of which this is a moderation of.'))
+      ->setRequired(TRUE);
+
+    $fields['created'] = BaseFieldDefinition::create('created')
+      ->setLabel(t('Created'))
+      ->setDescription(t('The first time that the entity was moderated'));
+
+    $fields['changed'] = BaseFieldDefinition::create('changed')
+      ->setLabel(t('Changed'))
+      ->setDescription(t('The last time that the entity was moderated.'));
+
+    $fields['data'] = BaseFieldDefinition::create('map')
+      ->setLabel(t('data'))
+      ->setDescription(t('Additional data.'))
+      ->setRequired(FALSE);
+
+    return $fields;
+  }
+
+}
---
+++ src/Entity/ModerationInterface.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Drupal\moderation\Entity;
+
+use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\Core\Entity\EntityChangedInterface;
+use Drupal\Core\Entity\EntityPublishedInterface;
+use Drupal\user\EntityOwnerInterface;
+
+/**
+ * Provides an interface for defining Moderation entities.
+ *
+ * @ingroup moderation
+ */
+interface ModerationInterface extends ContentEntityInterface, EntityChangedInterface, EntityPublishedInterface, EntityOwnerInterface {
+
+  /**
+   * Add get/set methods for your configuration properties here.
+   */
+
+  /**
+   * Gets the Moderation name.
+   *
+   * @return string
+   *   Name of the Moderation.
+   */
+  public function getName();
+
+  /**
+   * Sets the Moderation name.
+   *
+   * @param string $name
+   *   The Moderation name.
+   *
+   * @return \Drupal\moderation\Entity\ModerationInterface
+   *   The called Moderation entity.
+   */
+  public function setName($name);
+
+  /**
+   * Gets the Moderation creation timestamp.
+   *
+   * @return int
+   *   Creation timestamp of the Moderation.
+   */
+  public function getCreatedTime();
+
+  /**
+   * Sets the Moderation creation timestamp.
+   *
+   * @param int $timestamp
+   *   The Moderation creation timestamp.
+   *
+   * @return \Drupal\moderation\Entity\ModerationInterface
+   *   The called Moderation entity.
+   */
+  public function setCreatedTime($timestamp);
+
+}
